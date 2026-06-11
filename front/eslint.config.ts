13f7da6fb3453ebfed2a4d5cdd1bd4a3cd3179d3
Gabriel Kollet <gabrielkollet99@gmail.com>
2026-06-11 11:07:16 -0300

    chore: alter scaffold for TS and add linters

---
+++ front/eslint.config.ts
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+)
